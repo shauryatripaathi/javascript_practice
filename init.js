cc8c83c42243647c13b6431619f0b13368a0c6e4
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2022-03-17 13:34:20 +0530

    updated

--- init.js
+++ init.js
@@ -1,26 +1,12 @@
 let container = document.getElementById("container");
-let btn = document.createElement("button");
-btn.innerHTML = "Click here";
-container.appendChild(btn);
+let btn = document.getElementById("btn");
 
-let popUpBox = document.createElement("div");
-popUpBox.id = "popUpBox";
-container.appendChild(popUpBox);
-popUpBox.style.display = "none"
-
-let crossBtn = document.createElement("button");
-crossBtn.id = "crossBtn";
-crossBtn.innerText = "X";
-popUpBox.appendChild(crossBtn);
-
-function handleOnButton(){
-    btn.style.display = "none";
-    popUpBox.style.display = "block";
+function HandleOnClick(){
+    let x = Math.floor(Math.random()*256)
+    let y = Math.floor(Math.random()*256)
+    let z = Math.floor(Math.random()*256)
+    document.body.style.backgroundColor = `rgb(${x} ${y} ${z})`
+    console.log(document.body.style.backgroundColor)
+    console.log(x);
 }
-btn.addEventListener("click",handleOnButton)
-
-function handleOnCrossBtn(){
-    btn.style.display = "block";
-    popUpBox.style.display = "none";
-}
-crossBtn.addEventListener("click",handleOnCrossBtn);
+btn.addEventListener("click",HandleOnClick)

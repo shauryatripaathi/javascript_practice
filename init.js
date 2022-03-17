ee5d3f3aa4d995930f27fd43d721fd15fc6d8552
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2022-03-17 15:26:14 +0530

    Update init.js

--- init.js
+++ init.js
@@ -1,12 +1,31 @@
 let container = document.getElementById("container");
-let btn = document.getElementById("btn");
+let sideBar = document.createElement("div")
+sideBar.id = "sideBar";
+container.appendChild(sideBar);
 
-function HandleOnClick(){
-    let x = Math.floor(Math.random()*256)
-    let y = Math.floor(Math.random()*256)
-    let z = Math.floor(Math.random()*256)
-    document.body.style.backgroundColor = `rgb(${x} ${y} ${z})`
-    console.log(document.body.style.backgroundColor)
-    console.log(x);
+let sideBar_contents = document.createElement("ul");
+sideBar_contents.id = "sideBar_contents";
+sideBar.appendChild(sideBar_contents)
+
+let home_SideBar_Btn = document.createElement("li");
+home_SideBar_Btn.innerText = "Home";
+sideBar_contents.appendChild(home_SideBar_Btn);
+
+let about_SideBar_Btn = document.createElement("li");
+about_SideBar_Btn.innerText = "About";
+sideBar_contents.appendChild(about_SideBar_Btn);
+
+let contact_SideBar_Btn = document.createElement("li");
+contact_SideBar_Btn.innerText = "Contacts";
+sideBar_contents.appendChild(contact_SideBar_Btn);
+
+let home_DropBox = document.createElement("div");
+home_DropBox.id = "home_DropBox";
+
+function HandleOnHomeBtn(){
+   home_SideBar_Btn .appendChild(home_DropBox);
+    
 }
-btn.addEventListener("click",HandleOnClick)
+home_SideBar_Btn.addEventListener("click",HandleOnHomeBtn)
+
+

e68047a02479a4e0b9775b518dc58be3e4bab682
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2022-03-17 15:49:53 +0530

    Update init.js

--- init.js
+++ init.js
@@ -1,7 +1,9 @@
 let container = document.getElementById("container");
-let sideBar = document.createElement("div")
+let sideBar = document.createElement("div");
 sideBar.id = "sideBar";
 container.appendChild(sideBar);
+let dropBox = document.createElement("div");
+container.appendChild(dropBox);
 
 let sideBar_contents = document.createElement("ul");
 sideBar_contents.id = "sideBar_contents";
@@ -21,11 +23,21 @@ sideBar_contents.appendChild(contact_SideBar_Btn);
 
 let home_DropBox = document.createElement("div");
 home_DropBox.id = "home_DropBox";
+home_DropBox.style.display = "none";
+dropBox.appendChild(home_DropBox);
 
 function HandleOnHomeBtn(){
-   home_SideBar_Btn .appendChild(home_DropBox);
-    
-}
-home_SideBar_Btn.addEventListener("click",HandleOnHomeBtn)
+    if(home_DropBox.style.display == "none"){
+        home_DropBox.style.display = "block"
+    }
+    else{
+            home_DropBox.style.display = "none"
+        }
+    }
+home_SideBar_Btn.addEventListener("mouseover",HandleOnHomeBtn);
+
+home_DropBox.innerText = "News";
+
+
 
 

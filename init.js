4026cb540c38730c482ee523be652665e79340c4
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2022-03-23 00:20:03 +0530

    Update init.js

--- init.js
+++ init.js
@@ -2,42 +2,19 @@ let container = document.getElementById("container");
 let sideBar = document.createElement("div");
 sideBar.id = "sideBar";
 container.appendChild(sideBar);
-let dropBox = document.createElement("div");
-container.appendChild(dropBox);
 
-let sideBar_contents = document.createElement("ul");
-sideBar_contents.id = "sideBar_contents";
-sideBar.appendChild(sideBar_contents)
+let sideBarContents = document.createElement("ul");
+sideBarContents.id = "sideBarContents";
+sideBar.appendChild(sideBarContents);
 
-let home_SideBar_Btn = document.createElement("li");
-home_SideBar_Btn.innerText = "Home";
-sideBar_contents.appendChild(home_SideBar_Btn);
+let home_SideBarContent = document.createElement("li");
+home_SideBarContent.innerText = "Home";
+sideBarContents.appendChild(home_SideBarContent);
 
-let about_SideBar_Btn = document.createElement("li");
-about_SideBar_Btn.innerText = "About";
-sideBar_contents.appendChild(about_SideBar_Btn);
+let contacts_SideBarContent = document.createElement("li");
+contacts_SideBarContent.innerText = "contacts";
+sideBarContents.appendChild(contacts_SideBarContent);
 
-let contact_SideBar_Btn = document.createElement("li");
-contact_SideBar_Btn.innerText = "Contacts";
-sideBar_contents.appendChild(contact_SideBar_Btn);
-
-let home_DropBox = document.createElement("div");
-home_DropBox.id = "home_DropBox";
-home_DropBox.style.display = "none";
-dropBox.appendChild(home_DropBox);
-
-function HandleOnHomeBtn(){
-    if(home_DropBox.style.display == "none"){
-        home_DropBox.style.display = "block"
-    }
-    else{
-            home_DropBox.style.display = "none"
-        }
-    }
-home_SideBar_Btn.addEventListener("mouseover",HandleOnHomeBtn);
-
-home_DropBox.innerText = "News";
-
-
-
-
+let about_SideBarContent = document.createElement("li");
+about_SideBarContent.innerText = "About";
+sideBarContents.appendChild(about_SideBarContent);

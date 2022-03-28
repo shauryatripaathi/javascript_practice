4f1bfc3453ae9f4b547384a4c44fca20ea610243
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2022-03-28 14:08:56 +0530

    Update init.js

--- init.js
+++ init.js
@@ -1,20 +1,53 @@
 let container = document.getElementById("container");
-let sideBar = document.createElement("div");
-sideBar.id = "sideBar";
-container.appendChild(sideBar);
+// let sideBar = document.createElement("div");
+// sideBar.id = "sideBar";
+// container.appendChild(sideBar);
 
-let sideBarContents = document.createElement("ul");
-sideBarContents.id = "sideBarContents";
-sideBar.appendChild(sideBarContents);
+// let sideBarContents = document.createElement("ul");
+// sideBarContents.id = "sideBarContents";
+// sideBar.appendChild(sideBarContents);
 
-let home_SideBarContent = document.createElement("li");
-home_SideBarContent.innerText = "Home";
-sideBarContents.appendChild(home_SideBarContent);
+// let home_SideBarContent = document.createElement("li");
+// home_SideBarContent.innerText = "Home";
+// sideBarContents.appendChild(home_SideBarContent);
 
-let contacts_SideBarContent = document.createElement("li");
-contacts_SideBarContent.innerText = "contacts";
-sideBarContents.appendChild(contacts_SideBarContent);
+// let contacts_SideBarContent = document.createElement("li");
+// contacts_SideBarContent.innerText = "contacts";
+// sideBarContents.appendChild(contacts_SideBarContent);
 
-let about_SideBarContent = document.createElement("li");
-about_SideBarContent.innerText = "About";
-sideBarContents.appendChild(about_SideBarContent);
+// let about_SideBarContent = document.createElement("li");
+// about_SideBarContent.innerText = "About";
+// sideBarContents.appendChild(about_SideBarContent);
+
+
+let navBar = document.createElement("nav");
+navBar.className = "navBar";
+container.appendChild(navBar)
+
+let home = document.createElement('a');
+home.id = "home"
+home.appendChild(document.createTextNode('HOME'))
+home.setAttribute('href','#home')
+navBar.appendChild(home);
+
+let contact = document.createElement('a');
+contact.id = "contact";
+contact.setAttribute('href','#contact');
+contact.appendChild(document.createTextNode('CONTACTS'))
+navBar.appendChild(contact);
+
+let about = document.createElement('a')
+about.id = "about";
+about.setAttribute('href','#about');
+about.appendChild(document.createTextNode('ABOUT'))
+navBar.appendChild(about)
+
+let dropBox = document.createElement('div');
+dropBox.id = 'dropbox'
+dropBox.style.display = 'none'
+container.appendChild(dropBox)
+
+function handleOnHomeBtn(){
+    dropBox.style.display = 'block'
+}
+home.addEventListener("click",handleOnHomeBtn);
